9f497d7bd5cb8769104fb7976451ce9ae6132b1e
tasyaswantara <tasyaswantara@gmail.com>
2024-12-01 12:09:00 +0700

    feat: add logout behaviour

--- app/src/main/java/com/example/projekpapb2/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/projekpapb2/ui/screens/ProfileScreen.kt
@@ -15,13 +15,18 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.example.projekpapb2.R
 import com.example.projekpapb2.ui.components.BottomNavbar
 import com.example.projekpapb2.ui.theme.*
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 
 @Composable
 fun ProfileScreen(navController: NavController) {
+    val currentUser = Firebase.auth.currentUser
+    val profileImageUrl = currentUser?.photoUrl
     Scaffold(
         topBar = {
             Card(
@@ -63,23 +68,33 @@ fun ProfileScreen(navController: NavController) {
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_profile),
-                    contentDescription = "Profile Picture",
-                    modifier = Modifier
-                        .size(80.dp)
-                        .clip(CircleShape)
-                )
+                if (profileImageUrl != null) {
+                    AsyncImage(
+                        model = profileImageUrl.toString(),
+                        contentDescription = "Profile Picture",
+                        modifier = Modifier
+                            .size(80.dp)
+                            .clip(CircleShape)
+                    )
+                } else {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_profile),
+                        contentDescription = "Default Profile Picture",
+                        modifier = Modifier
+                            .size(80.dp)
+                            .clip(CircleShape)
+                    )
+                }
                 Spacer(modifier = Modifier.width(16.dp))
 
                 Column {
                     Text(
-                        text = "Keenan Tee",
+                        text = currentUser?.displayName ?: "Nama Tidak Diketahui",
                         style = TextStyle(fontFamily = Fredoka, fontSize = MaterialTheme.typography.titleLarge.fontSize),
                         color = Blue600
                     )
                     Text(
-                        text = "keenantee@gmail.com",
+                        text = currentUser?.email ?: "Email Tidak Tersedia",
                         style = TextStyle(fontFamily = Fredoka, fontSize = MaterialTheme.typography.bodyMedium.fontSize),
                         color = Blue200
                     )
@@ -89,18 +104,24 @@ fun ProfileScreen(navController: NavController) {
             Spacer(modifier = Modifier.height(24.dp))
             ProfileMenuItem(icon = R.drawable.ic_account, title = "Informasi Akun", "accountinfo", navController = navController)
             ProfileMenuItem(icon = R.drawable.ic_notification, title = "Notifikasi",null, navController = navController)
-            ProfileMenuItem(icon = R.drawable.ic_logout, title = "Keluar Akun",null, navController = navController)
+            ProfileMenuItem(icon = R.drawable.ic_logout, title = "Keluar Akun",null, navController = navController,onClick = {
+                Firebase.auth.signOut() // Logout dari Firebase
+                navController.navigate("login") { // Navigasi ke layar login
+                    popUpTo(0)}
+
+
+                })
         }
     }
 }
 
 @Composable
-fun ProfileMenuItem(icon: Int, title: String, navigate: String?, navController: NavController) {
+fun ProfileMenuItem(icon: Int, title: String, navigate: String?, navController: NavController, onClick: (() -> Unit)? = null) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .clickable {
-                navigate?.let {
+                onClick?.invoke() ?: navigate?.let {
                     navController.navigate(it)
                 }
             }
